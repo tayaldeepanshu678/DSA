0e892b5ed8183c1b093b31ab7fde463ccd60912b
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-07-24 19:07:47 +0530

    Time: 2094 ms (30.9%), Space: 98.6 MB (15.73%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        unordered_set<long long>st;
+        // unordered_set<long long>st;
         unordered_set<long long>ans;
         unordered_set<long long>ans_final;
-        for(int i = 0; i<nums.size(); i++){
-                        st.insert(nums[i]);
-                    // cout<<ans;
-                    // st.insert(ans);
-                }
+        // for(int i = 0; i<nums.size(); i++){
+        //                 st.insert(nums[i]);
+        //             // cout<<ans;
+        //             // st.insert(ans);
+        //         }
 
-        for(int p : st){
-            for(int n : nums){
-                ans.insert(p^n);
+        // for(int p : st){
+        //     for(int n : nums){
+        //         ans.insert(p^n);
+        //     }
+        // }
+        for(int i = 0; i<nums.size(); i++){
+            for(int j = i; j<nums.size(); j++){
+                ans.insert(nums[i]^nums[j]);
             }
         }
         for(int x : ans){
