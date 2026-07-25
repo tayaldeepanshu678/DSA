6c858527a833b95f0519fe52952ac645f209fe10
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-07-25 21:06:51 +0530

    Time: 23 ms (6.03%), Space: 12.4 MB (86.2%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        int count = 0;
+        for(int i = 0; i<nums1.size(); i++){
+            count = 0;
+            for(int j = 0; j<nums2.size(); j++){
+                if(nums1[i]==nums2[j]){
+                    for(int k = j+1; k<nums2.size(); k++){ 
+                    if(nums2[k]>nums2[j]){
+                        ans.push_back(nums2[k]);
+                        count = 1;
+                        break;
+                    }
+                    }
+                    if(count==0){
+                        ans.push_back(-1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
