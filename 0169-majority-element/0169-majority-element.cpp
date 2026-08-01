ac1951b6194b2c7cd0360396e37d2200de2c0e1c
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-01 22:05:38 +0530

    Time: 7 ms (10.8%), Space: 28.4 MB (12.75%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int>mpp;
+        int ans;
+        for(int i = 0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it : mpp){
+            if(it.second>(nums.size()/2)){
+                ans = it.first;
+            }
+        }
+        return ans;
+    }
+};
