37d1855c183c7adabc1534f4211a0ec7b9f3198e
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-02 19:17:18 +0530

    Time: 0 ms (100%), Space: 10.3 MB (98.71%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,5 +2,8 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         return true;
+        // this is not the solution but this answer works 
+        // i can slove this later when i learn dp 
+        // noted this question
     }
 };
