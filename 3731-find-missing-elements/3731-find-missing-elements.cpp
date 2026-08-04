b24dee7b8a93af1191f9239b7c8218741a837ae9
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-04 11:31:24 +0530

    Time: 8 ms (12.18%), Space: 34.4 MB (5.13%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
         vector<int>ans;
-        auto mini = *min_element(nums.begin(),nums.end());
-        auto maxi = *max_element(nums.begin(),nums.end());
+        // auto mini = *min_element(nums.begin(),nums.end());
+        // auto maxi = *max_element(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
         unordered_map<int,int>mpp;
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
         for(int x : nums){
+            mini = min(mini,x);
+            maxi = max(maxi,x);
             mpp[x]++;
         }
         for(int x = mini; x<=maxi; x++){
@@ -13,6 +18,9 @@ public:
                 ans.push_back(x);
             }
         }
+        // tc is o(n+p(traversing))
+        // sc is o(m(storing it in vector)+k(using mpp))
+        // doesn't need to sort them as they are running from mini to maxi;
         // sort(ans.begin(),ans.end());
         return ans;
     }
