929c0c642cb37fb20e68b6259f6220603ac74dd2
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-05 12:45:58 +0530

    Time: 7 ms (0.19%), Space: 9.7 MB (5.47%) - LeetHub

--- 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int theMaximumAchievableX(int num, int t) {
-        // int p = 2*t;
-        // while(p>0){
-        //     num++;
-        //     p--;
-        // }
-        // return num;
-        return 2*t+num;
+        int p = 2*t;
+        while(p>0){
+            num++;
+            p--;
+        }
+        return num;
+        // return 2*t+num;
     }
 };
