385ee3fba7ed0019df21bc7c9fd022ee8badb4ec
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-05 12:13:03 +0530

    Time: 0 ms (100%), Space: 17.5 MB (75.99%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -3,20 +3,29 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int ans = 0;
         int x = 0;
-        for(int i = 0; i<operations.size(); i++){
-            string ch = operations[i];
-            for(int j = 1; j<ch.size(); j++){
-                if(ch[j]=='-'){
-                   ans = --x;
-                   break;
+        // for(int i = 0; i<operations.size(); i++){
+        //     string ch = operations[i];
+        //     for(int j = 1; j<ch.size(); j++){
+        //         if(ch[j]=='-'){
+        //            ans = --x;
+        //            break;
                     
-                }
-                else{
-                   ans = ++x;
-                   break;
-                }
+        //         }
+        //         else{
+        //            ans = ++x;
+        //            break;
+        //         }
+        //     }
+        // }
+        for(string ch : operations){
+            if(ch[1]=='+'){
+                ++x;
+            }
+            else{
+                --x;
             }
         }
-        return ans;
+        return x;
+        // return ans;
     }
 };
