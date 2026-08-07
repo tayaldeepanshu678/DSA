2cd466949bbb9b0ef77dbe7672ff15dd65182b02
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-07 22:01:24 +0530

    Time: 0 ms (100%), Space: 11.8 MB (49.37%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        // int j = nums.size()-1;
-        // int ans = 0;
-        // int k;
-        // for(int i = 0; i<nums.size(); i++){
-        //     if(nums[i]==val && nums[j]!=val){
-        //         swap(nums[i],nums[j]);
-        //         j--;
-        //     }
-        //     else{
-        //         j--;
-        //     }
-        // }
-        // for(k = 0; k<nums.size(); k++){
-        //     if(nums[k]==val){
-        //         ans = k;
-        //         break;
-        //     }
-        // }
-        // return k-1;
-        vector<int>ans;
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]!=val){
-                ans.push_back(nums[i]);
+        int j = nums.size()-1;
+        int i = 0;
+        while(i<=j){
+            if(nums[i]==val){
+                swap(nums[i],nums[j]);
+                j--;
+            }
+            else{
+                i++;
             }
         }
-        nums = ans;
-       return nums.size();
+
+        return j+1;
+    //     vector<int>ans;
+    //     for(int i = 0; i<nums.size(); i++){
+    //         if(nums[i]!=val){
+    //             ans.push_back(nums[i]);
+    //         }
+    //     }
+    //     nums = ans;
+    //    return nums.size();
     }
 };
