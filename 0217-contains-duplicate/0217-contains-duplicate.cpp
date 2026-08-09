d92e014272916cfcfdf1dab5843ab18e19ebda53
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-09 18:59:58 +0530

    Time: 80 ms (20.28%), Space: 113.3 MB (16.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // map<int,int>mpp;
-        // for(int i : nums){
-        //     mpp[i]++;
-        // }
-        // for(auto it : mpp){
-        //     if(it.second >= 2){
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        sort(nums.begin(),nums.end());
-        for(int i = 1; i<nums.size(); i++){
-            if(nums[i]==nums[i-1]){
+        unordered_map<int,int>mpp;
+        for(int i : nums){
+            mpp[i]++;
+        }
+        for(auto it : mpp){
+            if(it.second >= 2){
                 return true;
             }
         }
         return false;
+
+        // sort(nums.begin(),nums.end());
+        // for(int i = 1; i<nums.size(); i++){
+        //     if(nums[i]==nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 };
