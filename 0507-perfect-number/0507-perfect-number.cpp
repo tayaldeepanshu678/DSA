45d08db4759331dd70d5e880b31e9461ec1bfaac
DEEPANSHU TAYAL <tayaldeepanshu678@gmail.com>
2026-08-14 21:41:02 +0530

    Time: 2107 ms (9.1%), Space: 7.8 MB (21.84%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-int div(int num){
-    int n = num;
-    int sum = 0;
-    for(int i = 1; i<num; i++){
-        if(num%i==0){
-            sum+=i;        }
-    }
-    if(sum==n){
-        return 1;
-    }
-    return 0;
-}
     bool checkPerfectNumber(int num) {
-        return div(num);
+        int n = num;
+        int sum = 0;
+        for(int i = 1; i<num; i++){
+            if(num%i==0){
+                sum+=i;        }
+        }
+        if(sum==n){
+            return 1;
+        }
+        return 0;
     }
 };
